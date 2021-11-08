7090aa5b42f0bb44ba17da8d8a1ed40921a579df
sambuckberry <sambuckberry@protonmail.com>
2021-11-08 20:06:07 +0800

    update with new pacakges

--- install-workshop-r-packages.R
+++ install-workshop-r-packages.R
@@ -7,11 +7,13 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 bioc_packages <- c('QuasR', 'Rsubread', 'GenomicRanges', 'GenomicAlignments',
   'GenomicFeatures', 'Rsamtools', 'rtracklayer', 'Rfastp',
   'edgeR', 'limma', 'ggbio', 'TxDb.Hsapiens.UCSC.hg38.knownGene',
-  'wiggleplotr', 'gprofiler2', 'biomaRt')
+  'wiggleplotr', 'gprofiler2', 'biomaRt', 'ensembldb', 'EnsDb.Hsapiens.v86')
 
 BiocManager::install(bioc_packages)
 
 ### Install from CRAN using bioc to ensure version compatability
-cran_package <-  c('stringr', 'magrittr', 'DT', 'markdown', 'mime', 'rmarkdown', 'tinytex', 'knitr', 'pheatmap', 'tidyr', 'ggplot2', 'ggrepel', 'upsetR', 'cowplot'. 'dplyr')
+cran_package <-  c('stringr', 'magrittr', 'DT', 'markdown', 'mime',
+                   'rmarkdown', 'tinytex', 'knitr', 'pheatmap', 'tidyr',
+                   'ggplot2', 'ggrepel', 'upsetR', 'cowplot'. 'dplyr')
 
 BiocManager::install(pkgs = cran_package, site_repository = "CRAN")
